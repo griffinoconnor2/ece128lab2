0d397278b22c46e03e4d7eec1c28e44de81cbb3b
Griffin O'Connor <griffinjoconnor@gmail.com>
2026-09-08 15:11:12 -0400

    new

--- RCA4bit.v
+++ RCA4bit.v
@@ -1,5 +1,9 @@
 module RCA4bit(input [3:0] A, B,
-               output [3:0] S
+               output [3:0] S,
+               output Cout;
 );
+
+assign S = A^B^C;
+
 
 endmodule
---
+++ RCAFA.v
@@ -0,0 +1,4 @@
+module RCAFA(input A, B, Cin,
+          output Cout, S);
+
+endmodule;
