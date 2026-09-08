ee8835bd2585522c2bd9c1d1a9c12f33bcbe5024
Griffin O'Connor <griffinjoconnor@gmail.com>
2026-09-08 15:26:58 -0400

    Finished 4-bit RCA design

--- RCA4bit.v
+++ RCA4bit.v
@@ -1,9 +1,15 @@
 module RCA4bit(input [3:0] A, B,
+               input Cin,
                output [3:0] S,
-               output Cout;
-);
+               output Cout);
 
-assign S = A^B^C;
+//Declare intermediate carries
+wire [3:1] C;
 
+//Implement 4-bit Ripple Carry Adder using RCAFA module
+RCAFA FA0 (.A(A[0]), .B(B[0]), .Cin(Cin),  .Cout(C[1]),  .S(S[0]));
+RCAFA FA1 (.A(A[1]), .B(B[1]), .Cin(C[1]), .Cout(C[2]),  .S(S[1]));
+RCAFA FA2 (.A(A[2]), .B(B[2]), .Cin(C[2]), .Cout(C[3]),  .S(S[2]));
+RCAFA FA3 (.A(A[3]), .B(B[3]), .Cin(C[3]), .Cout(Cout),  .S(S[3]));
 
 endmodule
